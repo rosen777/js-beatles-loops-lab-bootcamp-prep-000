28168013c22fcec20a0e9f7da7e75a9093f05666
Rosen Toshev <toshev@usc.edu>
2018-09-20 07:11:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,3 +30,7 @@ johnLennonFacts(["He was the last Beatle to learn to drive",
   "He was never a vegetarian",
   "He was a choir boy and boy scout",
   "He hated the sound of his own voice"])
+  
+  
+  
+  
